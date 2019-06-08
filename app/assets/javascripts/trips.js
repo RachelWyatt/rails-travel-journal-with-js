c95ece83a7fb25ddc0b47e696c7f7be16a7f0c7b
Rachel Wyatt <rw126@evansville.edu>
2019-06-08 18:44:54 -0400

    create Trip model object

--- app/assets/javascripts/trips.js
+++ app/assets/javascripts/trips.js
@@ -6,8 +6,19 @@ function attachListeners() {
     $('#all_trips').click(function() {
         fetch(`/trips.json`)
             .then(res => res.json())
-            .then(data => {
-                $('#trips-container').html('hi')
+            .then(trips => {
+                $('#trips-container').html('')
+                trips.forEach((trip) => {
+                    let newTrip = new Trip(trip)
+                    console.log(newTrip)
+                })
             })
     });
 } 
+
+
+function Trip(id, name, user) {
+    this.id = id 
+    this.name = name
+    this.user = user
+}
