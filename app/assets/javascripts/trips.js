0d0e2c3755df147581c245cde55556423edf2865
Rachel Wyatt <rw126@evansville.edu>
2019-06-23 11:28:39 -0400

    rewrite #all_trips event listener with ES6 syntax

--- app/assets/javascripts/trips.js
+++ app/assets/javascripts/trips.js
@@ -2,13 +2,13 @@ $(document).ready(function () {
     attachListeners()
 });
 
-function attachListeners() {
-    $('#all_trips').click(function(e) {
+const attachListeners = () => {
+    $('#all_trips').on('click', e => {
         e.preventDefault();
         fetch(`/trips.json`)
             .then(res => res.json())
             .then(trips => {
-                trips.forEach((trip) => {
+                trips.forEach(trip => {
                     let newTrip = new Trip(trip)
                     let postHtml = newTrip.formatIndex()
                     $('#trips-container').append(postHtml)
