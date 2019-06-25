55bed59b05d44b0b52bf60e31edc3a19cbbca163
Rachel Wyatt <rw126@evansville.edu>
2019-06-25 18:50:11 -0400

    project review coding challenge

--- app/assets/javascripts/trips.js
+++ app/assets/javascripts/trips.js
@@ -13,7 +13,7 @@ const attachListeners = () => {
                     let postHtml = newTrip.formatIndex()
                     $('#trips-container').append(postHtml)
                 })
-            })
+        })
     });
 
     $(document).on('click', "#trip_show", function(e) {
@@ -24,7 +24,7 @@ const attachListeners = () => {
             let newTrip = new Trip(trip)
             let journal = newTrip.formatJournalEntries()
             $('#trips-show-container').append(journal)
-    })
+        })
     })
 
     $('#new_location').on("submit", function(e) {
@@ -36,6 +36,20 @@ const attachListeners = () => {
                 $('#new_location_container').html(`<div class="col col-md-auto ml-md-auto"><li>${newLocation.name}</li></div>`)
             });
     });
+
+    $('#sort_alphabetically').on('click', function(e) {
+        fetch(`/trips.json`)
+        .then(res => res.json())
+        .then(trips => {
+            let tripArray = sortAlphabetically(trips);
+            console.log(tripArray)
+            tripArray.forEach(trip => {
+                let tripName = trip.name
+                console.log(tripName)
+                $('#trips-alphabet').append(tripName)
+            })
+        })
+    })
 } 
 
 function Location(location) {
@@ -55,6 +69,24 @@ Trip.prototype.formatIndex = function(){
     <h2 button type="button" data-id="${this.id}" class="btn btn-primary" id="trip_show"> ${this.name}</h2> 
     `
     return postHtml
+}
+
+function sortAlphabetically(trips){
+    let tripsSorted = trips.sort(function(a, b) {
+        var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+        var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+        if (nameA < nameB) {
+          return -1;
+        }
+        if (nameA > nameB) {
+          return 1;
+        }
+      
+        // names must be equal
+        return 0;
+      });
+    
+    return tripsSorted
 }
 
 Trip.prototype.formatJournalEntries = function() {
